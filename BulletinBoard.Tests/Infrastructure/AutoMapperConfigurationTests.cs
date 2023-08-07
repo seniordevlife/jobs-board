67b4cfebd8d58684d232636d193376e6aeb5f85e
unknown <oluochodhiambo11@gmail.com>
2023-08-07 20:39:53 +0300

    adding project files

---
+++ BulletinBoard.Tests/Infrastructure/AutoMapperConfigurationTests.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BulletinBoard.Infrastructure.AutoMapper;
+using Xunit;
+
+namespace BulletinBoard.Tests.Infrastructure
+{
+    public class AutoMapperConfigurationTests
+    {
+        [Fact (Skip = "Mapping profile is not finished yet")]
+        public void Configuration_IsValid()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new DefaultProfile()));
+            config.AssertConfigurationIsValid();
+        }
+    }
+}
